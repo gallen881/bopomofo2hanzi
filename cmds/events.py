d32953d58d42bd6eebd089ef20916edd12b820d8
Gallen Wang <gallen.yes@gmail.com>
2023-08-29 13:12:23 +0800

    update

--- cmds/events.py
+++ cmds/events.py
@@ -3,16 +3,25 @@ import discord
 
 from core.classes import Cog_Extension
 import core.function as function
-from utils import engTyping_end_fix, engTyping_rearrange, IsZhInputs
+from utils import engTyping_end_fix, engTyping_rearrange, IsZhInputs, text_classifier
 import translator_viterbi
 
 class Events(Cog_Extension):
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        message.content = engTyping_end_fix(engTyping_rearrange(message.content.lower()))
-        if IsZhInputs(message.content):
-            function.print_detail('INFO', message.author, message.guild, message.channel, f'Get message: {message.content}')
-            translated_msg = ''.join(translator_viterbi.decode_sentence(message.content)[1])
+        texts = []
+        text = engTyping_end_fix(engTyping_rearrange(message.content.lower()))
+        text_list = text_classifier(text)
+        if IsZhInputs(text):
+            texts = [text]
+        elif text_list[0]:
+            texts = text_list
+        if not texts:
+            output = []
+            for text in texts:
+                function.print_detail('INFO', message.author, message.guild, message.channel, f'Get message: {text}')
+                output.append(''.join(translator_viterbi.decode_sentence(text)[1]))
+            translated_msg = ' '.join(output)
             await message.reply(translated_msg)
             function.print_detail('INFO', message.author, message.guild, message.channel, f'Send message: {translated_msg}')
 
