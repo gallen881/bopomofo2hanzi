c806ffce1ad2ce1d5776b729c01bf13559185cd0
Gallen Wang <gallen.yes@gmail.com>
2023-08-30 22:26:01 +0800

    fix english but translate

--- cmds/events.py
+++ cmds/events.py
@@ -3,18 +3,20 @@ import discord
 
 from core.classes import Cog_Extension
 import core.function as function
-from utils import engTyping_end_fix, engTyping_rearrange, IsZhInputs, text_classifier
+from utils import engTyping_end_fix, engTyping_rearrange, IsZhInputs, text_classifier, engTyping_insert_split_char, split_char
 import translator_viterbi
 
 class Events(Cog_Extension):
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
+        if message.author == self.bot.user or message.content.startswith('-') or message.author.bot:
+            return
         texts = []
         text = engTyping_end_fix(message.content.lower())
         text_list = text_classifier(text) if len(text) >= 4 else ['']
         if IsZhInputs(text):
             texts = [engTyping_rearrange(text)]
-        elif text_list[0]:
+        elif len(max([engTyping_insert_split_char(part).split(split_char) for part in text_list], key=len)) >= 2:
             texts = text_list
         if texts:
             output = []
