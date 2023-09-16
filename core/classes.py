66c6260a547dec0694cf26b73302ea3de4a6cafa
Gallen Wang <gallen.yes@gmail.com>
2023-09-16 13:28:09 +0800

    test

--- core/classes.py
+++
@@ -1,5 +0,0 @@
-from discord.ext import commands
-
-class Cog_Extension(commands.Cog):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
--- fix_hanzi.py
+++
@@ -1,7 +0,0 @@
-from utils import zh2bopomofo, bopomofo2engTyping
-from translator_viterbi import decode_sentence
-
-while True:
-    text = input('?:')
-    decoded_sentence = decode_sentence(bopomofo2engTyping(''.join(zh2bopomofo(text))))[1]
-    print(''.join(decoded_sentence))
